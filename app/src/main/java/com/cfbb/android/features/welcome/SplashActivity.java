d0c0c31242897881d7bc4742ca61dfe0680c5554
yuchang <378434999@qq.com>
2016-10-26 10:27:42 +0800

    splash 优化

--- app/src/main/java/com/cfbb/android/features/welcome/SplashActivity.java
+++ app/src/main/java/com/cfbb/android/features/welcome/SplashActivity.java
@@ -31,12 +31,9 @@ import com.cfbb.android.features.update.UpdateAppService;
 import com.cfbb.android.features.update.UpdateDialog;
 import com.cfbb.android.features.webview.OtherActivity;
 import com.cfbb.android.protocol.APIException;
-import com.cfbb.android.protocol.APIService;
 import com.cfbb.android.protocol.RetrofitClient;
 import com.cfbb.android.protocol.YCNetSubscriber;
-import com.cfbb.android.protocol.bean.BaseResultBean;
 import com.cfbb.android.protocol.bean.ProductTypeBean;
-import com.cfbb.android.protocol.bean.UnsupportedBankCardBean;
 import com.cfbb.android.protocol.bean.UpdateVersionBean;
 import com.cfbb.android.protocol.bean.WelcomeInfoBean;
 import com.cfbb.android.protocol.downRequest.IProgressLisenr;
@@ -48,35 +45,44 @@ import java.util.List;
 
 /***
  * 启动页。引导页
+ * <p/>
+ * 1.进行系统版本检测
+ * 2.获取广告页
+ * 3.获取借款产品列表
+ * <p/>
+ * 要求，一定要等版本检测完成和广告加载成功 才能跳转。
  */
 public class SplashActivity extends BaseActivity implements View.OnClickListener, IProgressLisenr {
 
-    private ImageView iv_bg;
-    private CountDownTimer countDownTimer;
-    private CountDownTimer countDownTimerTwo;
-    private static final long LAEST_TIME = 7000;
     private static final long SHOW_PICLAEST_TIME = 5000;
+    private static int[] imagIds = {R.mipmap.one, R.mipmap.two,
+            R.mipmap.three};
+
+    private boolean isVersionOk = false;
+    private boolean isADShowOk = false;
+
+    private ImageView iv_bg;
     private ViewPager viewpager;
+    private Button btn_jump;
+
     private String currenVersionCode;
     private String preVersionCode;
     private YCDialogUtils ycDialogUtils;
 
-    //引导页
-    private int[] imagIds = {R.mipmap.one, R.mipmap.two,
-            R.mipmap.three};
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        StatService.onResume(this);
-        setBackEnable(false);
-    }
+    private CountDownTimer countDownTimer;
+    private CountDownTimer countDownTimerTwo;
+    private WelcomeInfoBean mWelcomeInfo;
+    private UpdateVersionBean updateVersionBean;
+    private UpdateAppService.MyBuinder myBuinder;
+    private Intent intent;
+    private UpdateDialog updateDialog;
 
     @Override
     protected void initContentView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_splash);
         currenVersionCode = AppUtils.getAppInfo(this).getVersionCode() + "";
         preVersionCode = (String) SPUtils.get(this, Const.IS_FIRST_START, "");
+        SPUtils.put(this, Const.IS_FIRST_START, currenVersionCode + "");
     }
 
     @Override
@@ -98,116 +104,52 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
 
             @Override
             public void onYCError(APIException e) {
-                ycDialogUtils.showSingleDialog(getResources().getString(R.string.dialog_title), "无法检测版本信息，请检查网络环境后重新打开应用！", new YCDialogUtils.MySingleBtnclickLisener() {
-                    @Override
-                    public void onBtnClick(View v) {
-                        ycDialogUtils.DismissMyDialog();
-                        finish();
-                    }
-                }, false);
+                FailToCheckVersion();
             }
 
             @Override
             public void onYcNext(UpdateVersionBean updateVersionBean) {
 
-                SplashActivity.this.updateVersionBean = updateVersionBean;
-                int currentVersionCode = AppUtils.getAppInfo(getApplicationContext()).getVersionCode();
-                if (currentVersionCode < updateVersionBean.version_code) {
-                    //需要更新版本
-                    final String downUrl = updateVersionBean.url;
-                    updateDialog = new UpdateDialog(SplashActivity.this);
-                    updateDialog.showDialog(updateVersionBean.version_desc, new UpdateDialog.MyBtnclickLisener() {
-                        @Override
-                        public void onOkClick(View v) {
-                            updateDialog.setBtnEnable(false);
-                            updateDialog.showPrograss();
-                            intent = new Intent(SplashActivity.this, UpdateAppService.class);
-                            bindService(intent, new ServiceConnection() {
-                                @Override
-                                public void onServiceConnected(ComponentName name, IBinder service) {
-                                    myBuinder = (UpdateAppService.MyBuinder) service;
-                                    myBuinder.setProgressLisener(SplashActivity.this);
-                                    myBuinder.startDown(downUrl);
-                                }
-
-                                @Override
-                                public void onServiceDisconnected(ComponentName name) {
-
-                                }
-                            }, BIND_AUTO_CREATE);
-                        }
-
-                        @Override
-                        public void onCancel(View v) {
-                            //广告执行完毕 点击关闭按钮直接跳转首页
-                            isVersionOk = true;
-                            if (isADShowOk) {
-                                TurnToMainActivity();
-                            }
-                        }
-
-                    }, false, updateVersionBean.force_update == 1 ? true : false);
-
-                } else {
-                    //不需要更新
-                    isVersionOk = true;
-                    if (isADShowOk) {
-                        TurnToMainActivity();
-                    }
-                }
+                CheckVersionOk(updateVersionBean);
             }
+
         });
 
-        //只要preVersionCode 和当前code不一致 就显示引导页,一致就显示启动页
+        RetrofitClient.getProductTypesRequest(null, this, new YCNetSubscriber<List<ProductTypeBean>>(this) {
+
+            @Override
+            public void onYCError(APIException e) {
+                SPUtils.put(SplashActivity.this, Const.PRODUCT_STR, "");
+            }
+
+            @Override
+            public void onYcNext(List<ProductTypeBean> model) {
+                Gson gson = new Gson();
+                SPUtils.put(SplashActivity.this, Const.PRODUCT_STR, gson.toJson(model));
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        StatService.onResume(this);
+        setBackEnable(false);
+        InitialVersionCheck();
+    }
+
+    private void InitialVersionCheck() {
+
         if (currenVersionCode.equals(preVersionCode)) {
 
-            //显示启动页
-            viewpager.setVisibility(View.GONE);
-            countDownTimer = new CountDownTimer(LAEST_TIME, 1000) {
-
-                @Override
-                public void onTick(long millisUntilFinished) {
-
-                }
-
-                @Override
-                public void onFinish() {
-                    isADShowOk = true;
-                    if (isVersionOk) {
-                        if (null != countDownTimerTwo) {
-                            countDownTimerTwo.cancel();
-                        }
-                        TurnToMainActivity();
-                    }
-                }
-            };
-            countDownTimer.start();
-
-            //TestResultUtils.getSussefulResult30()
             RetrofitClient.GetSplashInfo(null, this, new YCNetSubscriber<WelcomeInfoBean>(this) {
 
                 @Override
-                public void onYcNext(WelcomeInfoBean model) {
-                    mWelcomeInfo = model;
-                    if (!StrUtil.isEmpty(mWelcomeInfo.img)) {
-                        btn_jump = (Button) findViewById(R.id.btn_jump);
-                        ImageWithGlideUtils.lodeFromUrl(mWelcomeInfo.img, iv_bg, SplashActivity.this, new RequestListener() {
-
-                            @Override
-                            public boolean onException(Exception e, Object model, Target target, boolean isFirstResource) {
-                                return false;
-                            }
-
-                            @Override
-                            public boolean onResourceReady(Object resource, Object model, Target target, boolean isFromMemoryCache, boolean isFirstResource) {
-                                LoadOk();
-                                return false;
-                            }
-
-                        });
-                    } else {
-                        iv_bg.setImageResource((R.mipmap.start_bg));
-                    }
+                public void onYcNext(WelcomeInfoBean welcomeInfoBean) {
+//                    isADShowOk = true;
+                    GetAdOk(welcomeInfoBean);
                 }
 
                 @Override
@@ -216,11 +158,25 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
                 }
 
             });
+            countDownTimer = new CountDownTimer(SHOW_PICLAEST_TIME, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
 
+                }
+
+                @Override
+                public void onFinish() {
+                    isADShowOk = true;
+                    TurnToMainActivity();
+                }
+            };
+            countDownTimer.start();
+            //显示启动页
+            viewpager.setVisibility(View.GONE);
 
         } else {
 
-            SPUtils.put(this, Const.IS_FIRST_START, currenVersionCode + "");
+
             //显示引导页
             List<ImageView> datas = new ArrayList<>();
             for (int i = 0; i < imagIds.length; i++) {
@@ -233,24 +189,86 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
             viewpager.setAdapter(myViewPagerAdapor);
             viewpager.setVisibility(View.VISIBLE);
         }
+    }
 
+    private void GetAdOk(WelcomeInfoBean welcomeInfoBean) {
+        mWelcomeInfo = welcomeInfoBean;
+        if (!StrUtil.isEmpty(mWelcomeInfo.img)) {
+            btn_jump = (Button) findViewById(R.id.btn_jump);
+            ImageWithGlideUtils.lodeFromUrl(mWelcomeInfo.img, iv_bg, SplashActivity.this, new RequestListener() {
 
-        //TestResultUtils.getSussefulResult26()
-        RetrofitClient.getProductTypesRequest(null, this, new YCNetSubscriber<List<ProductTypeBean>>(this) {
+                @Override
+                public boolean onException(Exception e, Object model, Target target, boolean isFirstResource) {
+                    TurnToMainActivity();
+                    return false;
+                }
 
+                @Override
+                public boolean onResourceReady(Object resource, Object model, Target target, boolean isFromMemoryCache, boolean isFirstResource) {
+                    LoadOk();
+                    return false;
+                }
+
+            });
+        } else {
+            iv_bg.setImageResource((R.mipmap.start_bg));
+        }
+    }
+
+    private void CheckVersionOk(UpdateVersionBean updateVersionBean) {
+
+        SplashActivity.this.updateVersionBean = updateVersionBean;
+        int currentVersionCode = Integer.parseInt(currenVersionCode);
+        if (currentVersionCode < updateVersionBean.version_code) {
+            //需要更新版本
+            final String downUrl = updateVersionBean.url;
+            updateDialog = new UpdateDialog(SplashActivity.this);
+            updateDialog.showDialog(updateVersionBean.version_desc, new UpdateDialog.MyBtnclickLisener() {
+                @Override
+                public void onOkClick(View v) {
+                    updateDialog.setBtnEnable(false);
+                    updateDialog.showPrograss();
+                    intent = new Intent(SplashActivity.this, UpdateAppService.class);
+                    bindService(intent, new ServiceConnection() {
+                        @Override
+                        public void onServiceConnected(ComponentName name, IBinder service) {
+                            myBuinder = (UpdateAppService.MyBuinder) service;
+                            myBuinder.setProgressLisener(SplashActivity.this);
+                            myBuinder.startDown(downUrl);
+                        }
+
+                        @Override
+                        public void onServiceDisconnected(ComponentName name) {
+
+                        }
+                    }, BIND_AUTO_CREATE);
+                }
+
+                @Override
+                public void onCancel(View v) {
+                    isVersionOk = true;
+                    TurnToMainActivity();
+                }
+
+            }, false, updateVersionBean.force_update == 1 ? true : false);
+
+        } else {
+            //不需要更新
+            isVersionOk = true;
+            TurnToMainActivity();
+        }
+    }
+
+    private void FailToCheckVersion() {
+
+        ycDialogUtils.showSingleDialog(getResources().getString(R.string.dialog_title), "无法检测版本信息，请检查网络环境后重新打开应用！", new YCDialogUtils.MySingleBtnclickLisener() {
             @Override
-            public void onYCError(APIException e) {
-                //    super.onYCError(e);
-                SPUtils.put(SplashActivity.this, Const.PRODUCT_STR, "");
+            public void onBtnClick(View v) {
+                ycDialogUtils.DismissMyDialog();
+                finish();
             }
+        }, false);
 
-            @Override
-            public void onYcNext(List<ProductTypeBean> model) {
-                Gson gson = new Gson();
-                SPUtils.put(SplashActivity.this, Const.PRODUCT_STR, gson.toJson(model));
-            }
-
-        });
     }
 
     @Override
@@ -259,25 +277,20 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
         StatService.onPause(this);
     }
 
-
-    private WelcomeInfoBean mWelcomeInfo;
-    private Button btn_jump;
-
-    private boolean isVersionOk = false;
-    private boolean isADShowOk = false;
-
-    private UpdateVersionBean updateVersionBean;
-    private UpdateAppService.MyBuinder myBuinder;
-    private Intent intent;
-
-    private UpdateDialog updateDialog;
-
-
     private void TurnToMainActivity() {
-        Bundle bundle = new Bundle();
-        bundle.putInt(MainActivity.SHOW_FRAGMENT_INDEX, MainFragmentEnum.HOME.getValue());
-        JumpCenter.JumpActivity(SplashActivity.this, MainActivity.class, bundle, null, JumpCenter.NORMALL_REQUEST, JumpCenter.INVAILD_FLAG, true, false);
+        if (null != countDownTimerTwo) {
+            countDownTimerTwo.cancel();
+        }
+        if (null != countDownTimer) {
+            countDownTimer.cancel();
+        }
+        //确保广告请求获取ok 和 版本信息获取Ok
+        if (isADShowOk && isVersionOk) {
+            Bundle bundle = new Bundle();
+            bundle.putInt(MainActivity.SHOW_FRAGMENT_INDEX, MainFragmentEnum.HOME.getValue());
+            JumpCenter.JumpActivity(SplashActivity.this, MainActivity.class, bundle, null, JumpCenter.NORMALL_REQUEST, JumpCenter.INVAILD_FLAG, true, false);
 
+        }
     }
 
     private void LoadOk() {
@@ -287,6 +300,7 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
         btn_jump.setOnClickListener(SplashActivity.this);
         btn_jump.setText(SHOW_PICLAEST_TIME / 1000 + "  " + getResources().getString(R.string.jump));
         btn_jump.setVisibility(View.VISIBLE);
+
         countDownTimerTwo = new CountDownTimer(SHOW_PICLAEST_TIME, 1000) {
 
             @Override
@@ -298,6 +312,7 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
 
             @Override
             public void onFinish() {
+
                 btn_jump.setText(0 + "  " + getResources().getString(R.string.jump));
                 isADShowOk = true;
                 if (isVersionOk) {
@@ -310,7 +325,6 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
         };
         countDownTimerTwo.start();
     }
-
 
     @Override
     public void onClick(View v) {
@@ -339,16 +353,17 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
                 break;
             case R.id.btn_jump:
             case R.id.btn_ok:
-
-                if (updateVersionBean != null && isVersionOk) {
+                isADShowOk = true;
+                //if (updateVersionBean != null && isVersionOk) {
                     if (null != countDownTimerTwo) {
                         countDownTimerTwo.cancel();
                     }
                     if (null != countDownTimer) {
                         countDownTimer.cancel();
                     }
-                    TurnToMainActivity();
-                }
+
+             //   }
+                TurnToMainActivity();
                 break;
 
         }
@@ -359,14 +374,13 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
         int prograss = (int) (ArithUtil.div(bytesRead, contentLength, 2) * 100);
         updateDialog.setPrograss(prograss);
         if (prograss == 100) {
-            if (updateVersionBean.force_update == 0) {
+            if (updateVersionBean.force_update == -1) {
                 TurnToMainActivity();
             }
         }
 
     }
 
-    // 适配器
     private class MyViewPagerAdapor extends PagerAdapter {
 
         private List<ImageView> mList = new ArrayList<>();
@@ -380,31 +394,20 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
             return mList.size();
         }
 
-        /**
-         * 获得相应位置上的view container view的容器，其实就是viewpager自身 position 相应的位置
-         */
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             View view = mList.get(position);
-            // 到最后一个了。
             if ((position == getCount() - 1) && getCount() > 1) {
                 view.setId(R.id.btn_ok);
                 view.setOnClickListener(SplashActivity.this);
             }
-
-            // 给 container 添加一个view
             container.addView(mList.get(position));
-
-            // 返回一个和该view相对的object
             return mList.get(position);
         }
 
-        /**
-         * 判断 view和object的对应关系
-         */
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return view == object;// 官方提示这样写
+            return view == object;
         }
 
         @Override
